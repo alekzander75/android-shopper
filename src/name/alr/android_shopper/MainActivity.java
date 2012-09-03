ef72e8ef7e5e6d2a830d2b062d99d4b681d03e2e
alekzander <alopezruiz@gmail.com>
2012-09-03 17:35:11 -0300

    added format

--- src/name/alr/android_shopper/MainActivity.java
+++ src/name/alr/android_shopper/MainActivity.java
@@ -24,151 +24,148 @@ import android.widget.SimpleCursorAdapter;
  */
 public class MainActivity extends Activity {
 
-	private ShopperOpenHelper shopperOpenHelper;
+    private ShopperOpenHelper shopperOpenHelper;
 
-	private EditText itemEditText;
-	private boolean addingNew;
-	private ListView listView;
-	private BaseAdapter listAdapter;
-	private Cursor itemsCursor;
+    private EditText itemEditText;
+    private boolean addingNew;
+    private ListView listView;
+    private BaseAdapter listAdapter;
+    private Cursor itemsCursor;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-		this.shopperOpenHelper = new ShopperOpenHelper(this);
-		this.shopperOpenHelper.initialize();
+        this.shopperOpenHelper = new ShopperOpenHelper(this);
+        this.shopperOpenHelper.initialize();
 
-		this.itemsCursor = this.shopperOpenHelper.getItems();
+        this.itemsCursor = this.shopperOpenHelper.getItems();
 
-		startManagingCursor(this.itemsCursor);
+        startManagingCursor(this.itemsCursor);
 
-		this.listView = (ListView) findViewById(R.id.mainListView);
+        this.listView = (ListView) findViewById(R.id.mainListView);
 
-		this.listAdapter = new SimpleCursorAdapter(this, R.layout.main_list_entry,
-				this.itemsCursor, new String[] { ShopItem.NAME },
-				new int[] { R.id.mainListEntryText });
-		this.listView.setAdapter(this.listAdapter);
+        this.listAdapter = new SimpleCursorAdapter(this, R.layout.main_list_entry, this.itemsCursor,
+                new String[] { ShopItem.NAME }, new int[] { R.id.mainListEntryText });
+        this.listView.setAdapter(this.listAdapter);
 
-		this.listView.setItemsCanFocus(false);
-		this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        this.listView.setItemsCanFocus(false);
+        this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-		registerForContextMenu(this.listView);
+        registerForContextMenu(this.listView);
 
-		this.itemEditText = (EditText) findViewById(R.id.itemEditText);
+        this.itemEditText = (EditText) findViewById(R.id.itemEditText);
 
-		this.itemEditText.setOnKeyListener(new OnKeyListener() {
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if (event.getAction() == KeyEvent.ACTION_DOWN)
-					if (keyCode == KeyEvent.KEYCODE_ENTER) {
-						MainActivity.this.shopperOpenHelper.addItem(MainActivity.this.itemEditText.getText()
-								.toString().trim());
-						MainActivity.this.itemEditText.setText("");
-						MainActivity.this.itemsCursor.requery();
-						cancelAdd();
-						return true;
-					}
-				return false;
-			}
-		});
-	}
+        this.itemEditText.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                        MainActivity.this.shopperOpenHelper.addItem(MainActivity.this.itemEditText.getText().toString()
+                                .trim());
+                        MainActivity.this.itemEditText.setText("");
+                        MainActivity.this.itemsCursor.requery();
+                        cancelAdd();
+                        return true;
+                    }
+                return false;
+            }
+        });
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main_list_menu, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_list_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		super.onOptionsItemSelected(item);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
 
-		switch (item.getItemId()) {
-		case (R.id.add_item_menu_item): {
-			addNewItem();
-			return true;
-		}
-		case (R.id.remove_item_menu_item): {
-			if (this.addingNew) {
-				cancelAdd();
-			} else {
-				removeItem(this.listView.getSelectedItemId());
-			}
-			return true;
-		}
-		case (R.id.remove_all_items_menu_item): {
-			if (this.addingNew) {
-				cancelAdd();
-			} else {
-				removeAllItems();
-			}
-			return true;
-		}
-		}
+        switch (item.getItemId()) {
+        case (R.id.add_item_menu_item): {
+            addNewItem();
+            return true;
+        }
+        case (R.id.remove_item_menu_item): {
+            if (this.addingNew) {
+                cancelAdd();
+            } else {
+                removeItem(this.listView.getSelectedItemId());
+            }
+            return true;
+        }
+        case (R.id.remove_all_items_menu_item): {
+            if (this.addingNew) {
+                cancelAdd();
+            } else {
+                removeAllItems();
+            }
+            return true;
+        }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		super.onPrepareOptionsMenu(menu);
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
 
-		int idx = this.listView.getSelectedItemPosition();
+        int idx = this.listView.getSelectedItemPosition();
 
-		String removeTitle = getString(this.addingNew ? R.string.cancel : R.string.remove);
+        String removeTitle = getString(this.addingNew ? R.string.cancel : R.string.remove);
 
-		MenuItem removeItem = menu.findItem(R.id.remove_item_menu_item);
-		removeItem.setTitle(removeTitle);
-		removeItem.setVisible(this.addingNew || idx > -1);
+        MenuItem removeItem = menu.findItem(R.id.remove_item_menu_item);
+        removeItem.setTitle(removeTitle);
+        removeItem.setVisible(this.addingNew || idx > -1);
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
-		menu.setHeaderTitle("Selected Item");
-		MenuItem removeItemMenuItem = menu.add(0, R.id.remove_item_menu_item, Menu.NONE,
-				R.string.remove);
-		removeItemMenuItem.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			public boolean onMenuItemClick(MenuItem item) {
-				AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item
-						.getMenuInfo();
-				removeItem(menuInfo.id);
-				return true;
-			}
-		});
-	}
+        menu.setHeaderTitle("Selected Item");
+        MenuItem removeItemMenuItem = menu.add(0, R.id.remove_item_menu_item, Menu.NONE, R.string.remove);
+        removeItemMenuItem.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                removeItem(menuInfo.id);
+                return true;
+            }
+        });
+    }
 
-	private void removeAllItems() {
-		this.shopperOpenHelper.deleteAllItems();
-		MainActivity.this.itemsCursor.requery();
-	}
+    private void removeAllItems() {
+        this.shopperOpenHelper.deleteAllItems();
+        MainActivity.this.itemsCursor.requery();
+    }
 
-	private void addNewItem() {
-		this.addingNew = true;
-		this.itemEditText.setVisibility(View.VISIBLE);
-		this.itemEditText.requestFocus();
-	}
+    private void addNewItem() {
+        this.addingNew = true;
+        this.itemEditText.setVisibility(View.VISIBLE);
+        this.itemEditText.requestFocus();
+    }
 
-	private void removeItem(long id) {
-		this.shopperOpenHelper.deleteItem(id);
-		MainActivity.this.itemsCursor.requery();
-	}
+    private void removeItem(long id) {
+        this.shopperOpenHelper.deleteItem(id);
+        MainActivity.this.itemsCursor.requery();
+    }
 
-	@Override
-	protected void onDestroy() {
-		this.shopperOpenHelper.closeDatabase();
+    @Override
+    protected void onDestroy() {
+        this.shopperOpenHelper.closeDatabase();
 
-		super.onDestroy();
-	}
+        super.onDestroy();
+    }
 
-	private void cancelAdd() {
-		this.addingNew = false;
-		this.itemEditText.setVisibility(View.GONE);
-	}
+    private void cancelAdd() {
+        this.addingNew = false;
+        this.itemEditText.setVisibility(View.GONE);
+    }
 
 }
--- src/name/alr/android_shopper/database/ShopperOpenHelper.java
+++ src/name/alr/android_shopper/database/ShopperOpenHelper.java
@@ -11,76 +11,73 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class ShopperOpenHelper extends SQLiteOpenHelper {
 
-	private static final String[] GET_ITEMS__COLUMNS = new String[] { ShopItem.ID + " as _id",
-			ShopItem.NAME };
+    private static final String[] GET_ITEMS__COLUMNS = new String[] { ShopItem.ID + " as _id", ShopItem.NAME };
 
-	private SQLiteDatabase database;
+    private SQLiteDatabase database;
 
-	public ShopperOpenHelper(Context context) {
-		super(context, "main", null, 1);
-	}
+    public ShopperOpenHelper(Context context) {
+        super(context, "main", null, 1);
+    }
 
-	public void initialize() {
-		this.database = getWritableDatabase();
-	}
+    public void initialize() {
+        this.database = getWritableDatabase();
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE " + ShopItem.TABLE + " (" + ShopItem.ID
-				+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + ShopItem.NAME
-				+ " TEXT not null UNIQUE);");
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + ShopItem.TABLE + " (" + ShopItem.ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ShopItem.NAME + " TEXT not null UNIQUE);");
 
-		addTestData(db);
-	}
+        addTestData(db);
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// NOOP
-	}
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // NOOP
+    }
 
-	@Override
-	public void onOpen(SQLiteDatabase db) {
-		if (!db.isReadOnly()) {
-			// deleteAllItems(db);
-			// addTestData(db);
-		}
-	}
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        if (!db.isReadOnly()) {
+            // deleteAllItems(db);
+            // addTestData(db);
+        }
+    }
 
-	public void closeDatabase() {
-		this.database.close();
-	}
-	
-//	public static String getItemName(Cursor cursor) {
-//		return cursor.getString(cursor.getColumnIndexOrThrow(ShopItem.NAME));
-//	}
+    public void closeDatabase() {
+        this.database.close();
+    }
 
-	public Cursor getItems() {
-		return this.database.query(ShopItem.TABLE, GET_ITEMS__COLUMNS, null, null, null, null,
-				ShopItem.NAME + " COLLATE NOCASE");
-		// return db.query(ShopItem.ITEM_TABLE, null, null, null, null, null, ShopItem.NAME_COLUMN);
-	}
-	
-	public void addItem(String name) {
-		ContentValues contentValues = new ContentValues();
-		contentValues.put(ShopItem.NAME, name);
-		this.database.insertOrThrow(ShopItem.TABLE, null, contentValues);
-	}
-	
-	public void deleteAllItems() {
-		this.database.delete(ShopItem.TABLE, null, null);
-	}
+    // public static String getItemName(Cursor cursor) {
+    // return cursor.getString(cursor.getColumnIndexOrThrow(ShopItem.NAME));
+    // }
 
-	public void deleteItem(long id) {
-		this.database.delete(ShopItem.TABLE, ShopItem.ID + " = ?",
-				new String[] { Long.toString(id) });
-	}
+    public Cursor getItems() {
+        return this.database.query(ShopItem.TABLE, GET_ITEMS__COLUMNS, null, null, null, null, ShopItem.NAME
+                + " COLLATE NOCASE");
+        // return db.query(ShopItem.ITEM_TABLE, null, null, null, null, null, ShopItem.NAME_COLUMN);
+    }
 
-	private static void addTestData(SQLiteDatabase db) {
-		ContentValues contentValues = new ContentValues();
-		contentValues.put(ShopItem.NAME, "Carrots");
-		db.insertOrThrow(ShopItem.TABLE, null, contentValues);
-		contentValues.put(ShopItem.NAME, "Tomatoes");
-		db.insertOrThrow(ShopItem.TABLE, null, contentValues);
-	}
+    public void addItem(String name) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ShopItem.NAME, name);
+        this.database.insertOrThrow(ShopItem.TABLE, null, contentValues);
+    }
+
+    public void deleteAllItems() {
+        this.database.delete(ShopItem.TABLE, null, null);
+    }
+
+    public void deleteItem(long id) {
+        this.database.delete(ShopItem.TABLE, ShopItem.ID + " = ?", new String[] { Long.toString(id) });
+    }
+
+    private static void addTestData(SQLiteDatabase db) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ShopItem.NAME, "Carrots");
+        db.insertOrThrow(ShopItem.TABLE, null, contentValues);
+        contentValues.put(ShopItem.NAME, "Tomatoes");
+        db.insertOrThrow(ShopItem.TABLE, null, contentValues);
+    }
 
 }
