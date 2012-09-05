344c17b96602fe1cda2350b0f532886452664d43
alekzander <alopezruiz@gmail.com>
2012-09-05 12:08:16 -0300

    used dialogs

--- src/name/alr/android_shopper/MainActivity.java
+++ src/name/alr/android_shopper/MainActivity.java
@@ -3,8 +3,14 @@ package name.alr.android_shopper;
 import name.alr.android_shopper.database.ShopItem;
 import name.alr.android_shopper.database.ShopperOpenHelper;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
@@ -15,6 +21,7 @@ import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -24,13 +31,15 @@ import android.widget.SimpleCursorAdapter;
  */
 public class MainActivity extends Activity {
 
+    private static final int ADD_ITEM_DIALOG_ID = 1;
+
     private ShopperOpenHelper shopperOpenHelper;
 
-    private EditText itemEditText;
-    private boolean addingNew;
     private ListView listView;
     private BaseAdapter listAdapter;
     private Cursor itemsCursor;
+    private EditText addItemEditText;
+    private Button okButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,23 +64,11 @@ public class MainActivity extends Activity {
         this.listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         registerForContextMenu(this.listView);
+    }
 
-        this.itemEditText = (EditText) findViewById(R.id.itemEditText);
-
-        this.itemEditText.setOnKeyListener(new OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.ACTION_DOWN)
-                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                        MainActivity.this.shopperOpenHelper.addItem(MainActivity.this.itemEditText.getText().toString()
-                                .trim());
-                        MainActivity.this.itemEditText.setText("");
-                        MainActivity.this.itemsCursor.requery();
-                        cancelAdd();
-                        return true;
-                    }
-                return false;
-            }
-        });
+    private void addItem(String name) {
+        this.shopperOpenHelper.addItem(name);
+        this.itemsCursor.requery();
     }
 
     @Override
@@ -86,23 +83,15 @@ public class MainActivity extends Activity {
 
         switch (item.getItemId()) {
         case (R.id.add_item_menu_item): {
-            addNewItem();
+            showDialog(ADD_ITEM_DIALOG_ID);
             return true;
         }
         case (R.id.remove_item_menu_item): {
-            if (this.addingNew) {
-                cancelAdd();
-            } else {
-                removeItem(this.listView.getSelectedItemId());
-            }
+            removeItem(this.listView.getSelectedItemId());
             return true;
         }
         case (R.id.remove_all_items_menu_item): {
-            if (this.addingNew) {
-                cancelAdd();
-            } else {
-                removeAllItems();
-            }
+            removeAllItems();
             return true;
         }
         }
@@ -116,11 +105,11 @@ public class MainActivity extends Activity {
 
         int idx = this.listView.getSelectedItemPosition();
 
-        String removeTitle = getString(this.addingNew ? R.string.cancel : R.string.remove);
+        String removeTitle = getString(R.string.remove);
 
         MenuItem removeItem = menu.findItem(R.id.remove_item_menu_item);
         removeItem.setTitle(removeTitle);
-        removeItem.setVisible(this.addingNew || idx > -1);
+        removeItem.setVisible(idx > -1);
 
         return true;
     }
@@ -145,12 +134,6 @@ public class MainActivity extends Activity {
         MainActivity.this.itemsCursor.requery();
     }
 
-    private void addNewItem() {
-        this.addingNew = true;
-        this.itemEditText.setVisibility(View.VISIBLE);
-        this.itemEditText.requestFocus();
-    }
-
     private void removeItem(long id) {
         this.shopperOpenHelper.deleteItem(id);
         MainActivity.this.itemsCursor.requery();
@@ -163,9 +146,87 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
-    private void cancelAdd() {
-        this.addingNew = false;
-        this.itemEditText.setVisibility(View.GONE);
+    @Override
+    protected Dialog onCreateDialog(int id, Bundle args) {
+        switch (id) {
+        case ADD_ITEM_DIALOG_ID:
+            AlertDialog.Builder addItemDialogBuilder = new AlertDialog.Builder(this);
+            addItemDialogBuilder.setIcon(R.drawable.add_new_item);
+            addItemDialogBuilder.setTitle(getString(R.string.add_new_item));
+            View view = getLayoutInflater().inflate(R.layout.add_item_dialog_layout, null);
+            addItemDialogBuilder.setView(view);
+
+            this.addItemEditText = (EditText) view.findViewById(R.id.addItemEditText);
+
+            this.addItemEditText.setOnKeyListener(new OnKeyListener() {
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                            String itemName = getTrimmedString(getAddItemEditText());
+                            if (!itemName.isEmpty()) {
+                                dismissDialog(ADD_ITEM_DIALOG_ID);
+                                addItem(itemName);
+                            } else {
+                                getAddItemEditText().requestFocus();
+                            }
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+            });
+
+            this.addItemEditText.addTextChangedListener(new TextWatcher() {
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    // NOOP
+                }
+
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    // NOOP
+                }
+
+                public void afterTextChanged(Editable editable) {
+                    MainActivity.this.okButton.setEnabled(!getTrimmedString(editable).isEmpty());
+                }
+            });
+
+            addItemDialogBuilder.setPositiveButton(android.R.string.ok, new OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    addItem(getTrimmedString(getAddItemEditText()));
+                }
+            });
+            addItemDialogBuilder.setNegativeButton(android.R.string.cancel, null);
+
+            return addItemDialogBuilder.create();
+        }
+        return null;
     }
+
+    private String getTrimmedString(EditText editText) {
+        return getTrimmedString(editText.getText());
+    }
+
+    private String getTrimmedString(Editable editable) {
+        return editable.toString().trim();
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog, Bundle args) {
+        switch (id) {
+        case ADD_ITEM_DIALOG_ID:
+            AlertDialog addItemDialog = (AlertDialog) dialog;
+            if (this.okButton == null) {
+                this.okButton = addItemDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            }
+            getAddItemEditText().setText("");
+
+            break;
+        }
+    }
+
+    private EditText getAddItemEditText() {
+        return this.addItemEditText;
+    };
 
 }
